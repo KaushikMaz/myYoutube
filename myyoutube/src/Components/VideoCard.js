2790eb22ba31a5fd2b73bb1b3708b65e46eae101
Kaushik <kmzdr1@gmail.com>
2023-07-15 11:48:23 +0530

    update: cursor-pointer in videocard

--- myyoutube/src/Components/VideoCard.js
+++ myyoutube/src/Components/VideoCard.js
@@ -6,7 +6,7 @@ const VideoCard = ({info}) => {
     const {thumbnails,channelTitle, title}=snippet
         
   return (
-    <div className="p-1 m-1 w-80">
+    <div className="p-1 m-1 w-80 cursor-pointer">
         <img className="rounded-lg" alt= "Thumbnail" src={thumbnails.medium.url}/>
         <h1 className="font-semiBold line-clamp-2 overflow-hidden">{title}</h1>
         <h6 className="text-gray-500">{channelTitle}</h6>
