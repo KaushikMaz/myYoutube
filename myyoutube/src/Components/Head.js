bb89b2779027742c9b1363e4860e07f65aae7961
Kaushik <kmzdr1@gmail.com>
2023-08-12 20:46:57 +0530

    Feat: Closure of suggestionBox if clicked outside

--- myyoutube/src/Components/Head.js
+++ myyoutube/src/Components/Head.js
@@ -25,6 +25,22 @@ const handleSearchQuery=(e)=>{
     setSearchQuery(searchInput)
     searchInput?setShowSuggestion(true):setShowSuggestion(false)
 }
+
+const handleClickOutside=(e)=>{
+    const isSearchBoxClicked=e.target.closest(".searchInput")
+    const isSuggestionBoxClicked=e.target.closest(".suggestionBox")
+    if(!isSearchBoxClicked && !isSuggestionBoxClicked){
+        setShowSuggestion(false)
+    }
+
+}
+/* To recognise click events beyond searchBox and suggestion box*/
+React.useEffect(()=>{
+    document.addEventListener("click",handleClickOutside)
+
+    return()=>document.removeEventListener("click",handleClickOutside)
+},[])
+
     React.useEffect(()=>{
        
         const timer=setTimeout(()=>{
@@ -84,7 +100,7 @@ const handleSearchQuery=(e)=>{
             </div>
             <div className="col-span-10 px-8">
                 <div className="flex align-center">
-                    <input className="w-3/4 border border-gray-300 p-1 pl-3 rounded-l-full" type="text" value={searchQuery}  onChange={handleSearchQuery} placeholder="Search here"/>
+                    <input className=" searchInput w-3/4 border border-gray-300 p-1 pl-3 rounded-l-full" type="text" value={searchQuery}  onChange={handleSearchQuery} placeholder="Search here"/>
                     <button onClick={()=>{setIsSearchClick(true); setShowSuggestion(false)}} className="px-3 border border-gray-300 p-1 rounded-r-full bg-gray-100">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
     <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z" clipRule="evenodd" />
@@ -93,7 +109,7 @@ const handleSearchQuery=(e)=>{
                     
                 </div>
                 {showSuggestion &&
-                <div className= "fixed z-20 bg-white py-2 px-2 w-[49.5rem] border border-gray-100 shadow-md mt-0 rounded-lg">
+                <div className= "suggestionBox fixed z-20 bg-white py-2 px-2 w-[49.5rem] border border-gray-100 shadow-md mt-0 rounded-lg">
                     <ul>
                         {suggestions.map((s, index)=><li onClick={()=>{setSearchQuery(s); setIsSearchClick(true); setShowSuggestion(false)}} className="py-2 px-3 hover:bg-gray-300 rounded-lg cursor-pointer " key={index}>{s}</li>)}
                         
